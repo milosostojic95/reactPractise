ea6d4cf3913ae142b6d707b2528d1a84fee434b0
Milos Ostojic <ostoja_95_cacak@hotmail.com>
2020-06-17 14:32:51 +0200

    Toggled on click. show persons

--- src/App.js
+++ src/App.js
@@ -5,30 +5,52 @@ import Person from './Person/Person';
 import UserInput from './UserInput/UserInput';
 import UserOutput from './UserOutput/UserOutput';
 
-
-
-
 class App extends Component {
   state = {
-    username: 'supermax',
+    persons: [
+      {name: 'Milos'},
+      {name: 'Gole'}
+    ],
+    showPersons: false
   }
 
-  inputChangeHeandler = (event) => {
+  togglePersonsHandler = () => {
+    const doesShow = this.state.showPersons;
     this.setState({
-      username: event.target.value
-      }
-    );
+      showPersons: !doesShow,
+    });
   }
 
-  render() {
-    return (
-      <div className="App">
-
-        <UserInput changed={this.inputChangeHeandler} currentName={this.state.username}/>
-        <UserOutput userName={this.state.username}/>
-        <UserOutput userName="milos"/>
-      </div>
-    );
+  changeNameHandler = (event) => {
+    this.setState({
+      persons: [
+        {name: event.target.value},
+        {name: 'Mare'}
+      ]
+    })
   }
+
+  render () {
+   return (
+    <div className="App">
+    <h1>Ide React Gas</h1>
+    <button onClick={this.togglePersonsHandler}>Change Name</button>
+    { this.state.showPersons ?
+      <div>
+        <Person
+          name={this.state.persons[0].name}
+          click={this.switchNameHandler}
+          change={this.changeNameHandler}/>
+        <Person
+          name={this.state.persons[1].name}>This is best lengauge
+        </Person>
+      </div> : null
+    }
+    <UserInput/>
+    <UserOutput userName="milos"/>
+    <UserOutput userName="milos"/>
+  </div>
+  );
+ }
 }
 export default App;
